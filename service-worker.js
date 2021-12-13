b79c46029ca550908c6c881736a7d8463da75c3d
Dzeb <grour@inbox.ru>
2021-12-13 23:41:07 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo-social-app/precache-manifest.d4d1fccbaf35d0759fb62de5266f6f26.js"
+  "/demo-social-app/precache-manifest.195781b6b7f9dee6f3f2cca303604b35.js"
 );
 
 self.addEventListener('message', (event) => {
